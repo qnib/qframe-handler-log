990fdb71bf44b0bd3814f60ddc89db60515b85f6
Christian Kniep <christian@qnib.org>
2017-05-01 10:39:37 +0200

    adjust log-handler to updated plugin framework

--- lib/log.go
+++ lib/log.go
@@ -32,18 +32,21 @@ func New(qChan qtypes.QChan, cfg config.Config, name string) Plugin {
 
 // Run fetches everything from the Data channel and flushes it to stdout
 func (p *Plugin) Run() {
-	log.Printf("[II] Start log handler v%s", version)
+	log.Printf("[II] Start log handler %sv%s", p.Name, version)
 	bg := p.QChan.Data.Join()
-	inStr, err := p.Cfg.String("handler.log.inputs")
+	inStr, err := p.Cfg.String(fmt.Sprintf("handler.%s.inputs", p.Name))
 	if err != nil {
 		inStr = ""
 	}
 	inputs := strings.Split(inStr, ",")
+	srcSuccess, err := p.Cfg.BoolOr(fmt.Sprintf("handler.%s.source-success", p.Name), true)
 	for {
 		val := bg.Recv()
 		qm := val.(qtypes.QMsg)
-		if len(inputs) != 0 && ! qutils.IsInput(inputs, qm.Source) {
-			//fmt.Printf("%s %-7s sType:%-6s sName:%-10s[%d] DROPED : %s\n", qm.TimeString(), qm.LogString(), qm.Type, qm.Source, qm.SourceID, qm.Msg)
+		if len(inputs) != 0 && ! qutils.IsLastSource(inputs, qm.Source) {
+			continue
+		}
+		if qm.SourceSuccess != srcSuccess {
 			continue
 		}
 		fmt.Printf("%s %-7s sType:%-6s sName:[%d]%-10s %s\n", qm.TimeString(), qm.LogString(), qm.Type, qm.SourceID, qm.Source, qm.Msg)
